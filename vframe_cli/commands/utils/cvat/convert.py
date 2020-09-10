3a036ce65a48c17e70ac8cd0dec757f88ea9e4e5
adamhrv <adam@ahprojects.com>
2020-09-10 23:08:39 +0200

    fix indent

--- vframe_cli/commands/utils/cvat/convert.py
+++ vframe_cli/commands/utils/cvat/convert.py
@@ -38,59 +38,57 @@ def cli(ctx, opt_input, opt_output):
     pipe_header = PipeContextHeader.from_dict(item)
     log.debug(pipe_header._frames_data)
 
-
- """
- <?xml version="1.0" encoding="utf-8"?>
-<annotations>
-  <version>1.1</version>
-  <meta>
-    <task>
-      <id>21</id>
-      <name>new task</name>
-      <size>288</size>
-      <mode>interpolation</mode>
-      <overlap>5</overlap>
-      <bugtracker></bugtracker>
-      <created>2020-07-03 10:19:35.173991+00:00</created>
-      <updated>2020-07-03 10:20:18.127172+00:00</updated>
-      <start_frame>0</start_frame>
-      <stop_frame>287</stop_frame>
-      <frame_filter></frame_filter>
-      <z_order>False</z_order>
-      <labels>
-        <label>
-          <name>face</name>
-          <attributes>
-          </attributes>
-        </label>
-      </labels>
-      <segments>
-        <segment>
-          <id>8</id>
-          <start>0</start>
-          <stop>287</stop>
-          <url>http://localhost:8080/?id=8</url>
-        </segment>
-      </segments>
-      <owner>
-        <username>vframe</username>
-        <email></email>
-      </owner>
-      <assignee></assignee>
-      <original_size>
-        <width>320</width>
-        <height>240</height>
-      </original_size>
-    </task>
-    <dumped>2020-07-03 10:21:46.421037+00:00</dumped>
-    <source>c6BGAl561No.mp4</source>
-  </meta>
-  <track id="0" label="face">
-    <box frame="0" outside="0" occluded="0" keyframe="1" xtl="115.72" ytl="78.10" xbr="220.16" ybr="163.69">
-    </box>
-    <box frame="1" outside="1" occluded="0" keyframe="1" xtl="115.72" ytl="78.10" xbr="220.16" ybr="163.69">
-    </box>
-  </track>
-</annotations>
 """
->>>>>>> dcd4864a6947464bd6b7e863bfb5f5c5ca668f45
+   <?xml version="1.0" encoding="utf-8"?>
+  <annotations>
+    <version>1.1</version>
+    <meta>
+      <task>
+        <id>21</id>
+        <name>new task</name>
+        <size>288</size>
+        <mode>interpolation</mode>
+        <overlap>5</overlap>
+        <bugtracker></bugtracker>
+        <created>2020-07-03 10:19:35.173991+00:00</created>
+        <updated>2020-07-03 10:20:18.127172+00:00</updated>
+        <start_frame>0</start_frame>
+        <stop_frame>287</stop_frame>
+        <frame_filter></frame_filter>
+        <z_order>False</z_order>
+        <labels>
+          <label>
+            <name>face</name>
+            <attributes>
+            </attributes>
+          </label>
+        </labels>
+        <segments>
+          <segment>
+            <id>8</id>
+            <start>0</start>
+            <stop>287</stop>
+            <url>http://localhost:8080/?id=8</url>
+          </segment>
+        </segments>
+        <owner>
+          <username>vframe</username>
+          <email></email>
+        </owner>
+        <assignee></assignee>
+        <original_size>
+          <width>320</width>
+          <height>240</height>
+        </original_size>
+      </task>
+      <dumped>2020-07-03 10:21:46.421037+00:00</dumped>
+      <source>c6BGAl561No.mp4</source>
+    </meta>
+    <track id="0" label="face">
+      <box frame="0" outside="0" occluded="0" keyframe="1" xtl="115.72" ytl="78.10" xbr="220.16" ybr="163.69">
+      </box>
+      <box frame="1" outside="1" occluded="0" keyframe="1" xtl="115.72" ytl="78.10" xbr="220.16" ybr="163.69">
+      </box>
+    </track>
+  </annotations>
+"""
